82bdea48624205422449f3ba648901cdd5bdb361
Gregory Kedge <gregwork@kedges.com>
2016-02-11 21:21:28 -0500

    Added race x age stacked bar chart.

--- gulpfile.js
+++ gulpfile.js
@@ -15,119 +15,117 @@ var del = require('del');
 var reload = browserSync.reload;
 
 var config = {
-  paths: {
-    app:   'src',
-    build: 'dist'
-  }
+    paths: {
+        app  : 'src',
+        build: 'dist'
+    }
 };
 
 // Images
-gulp.task('images:clean', function(next) {
-  del(config.paths.build + '/img/**', next);
+gulp.task('images:clean', function (next) {
+    del(config.paths.build + '/img/**', next);
 });
-gulp.task('images', ['images:clean'], function() {
-  return gulp.src(config.paths.app + '/img/**/*')
-    .pipe(gulp.dest(config.paths.build +'/img'));
+gulp.task('images', ['images:clean'], function () {
+    return gulp.src(config.paths.app + '/img/**/*')
+        .pipe(gulp.dest(config.paths.build + '/img'));
 });
 
 // Styles
-gulp.task('styles:clean', function(next) {
-  del(config.paths.build + '/css/**', next);
+gulp.task('styles:clean', function (next) {
+    del(config.paths.build + '/css/**', next);
 });
-gulp.task('styles', ['styles:clean'], function() {
-  return gulp.src(config.paths.app + '/css/**/*')
-    .pipe(gulp.dest(config.paths.build + '/css'));
+gulp.task('styles', ['styles:clean'], function () {
+    return gulp.src(config.paths.app + '/css/**/*')
+        .pipe(gulp.dest(config.paths.build + '/css'));
 });
 
 // Javascript
-gulp.task('js:clean', function(next) {
-  del(config.paths.build + '/js/**', next);
+gulp.task('js:clean', function (next) {
+    del(config.paths.build + '/js/**', next);
 });
-gulp.task('js', ['js:clean'], function() {
-  return gulp.src(config.paths.app + '/js/**/*')
-    .pipe(gulpif(gutil.env.env === 'production',
-      function() {
-        // TODO: Data obtained from local CSV.
-        
-        if( process.env.SOCRATA_APP_TOKEN ) {
-          return replace('bjp8KrRvAPtuf809u1UXnI0Z8',process.env.SOCRATA_APP_TOKEN);
-        } else {
-          gutil.log(gutils.colors.red('You need to set the SOCRATA_APP_TOKEN environment variable!'));
-          gutil.beep();
-          return gutil.noop();
-        }                                                                
-      }
-    ))
-    .pipe(gulp.dest(config.paths.build + '/js'));
+gulp.task('js', ['js:clean'], function () {
+    return gulp.src(config.paths.app + '/js/**/*')
+        .pipe(gulpif(gutil.env.env === 'production',
+            function () {
+                // TODO: Data obtained from local CSV.
+            }
+        ))
+        .pipe(gulp.dest(config.paths.build + '/js'));
 });
 
-gulp.task('bower', function() {
-  return bower()
-    .pipe(gulp.dest(config.paths.build + '/bower_components'));
+gulp.task('bower', function () {
+    return bower()
+        .pipe(gulp.dest(config.paths.build + '/bower_components'));
 });
 
-gulp.task('wiredep', function() {
-  return gulp.src('src/*.html')
-    .pipe(wiredep())
-    .pipe(gulp.dest('src'));
+gulp.task('wiredep', function () {
+    return gulp.src('src/index.html')
+        .pipe(wiredep())
+        .pipe(gulp.dest('src'));
+});
+
+gulp.task('wiredepDist', function () {
+    return gulp.src('dist/index.html')
+        .pipe(wiredep())
+        .pipe(gulp.dest('dist'));
 });
 
 // HTML
-gulp.task('html:clean', function(next) {
-  del(config.paths.build + '/**/*.html', next);
+gulp.task('html:clean', function (next) {
+    del(config.paths.build + '/**/*.html', next);
 });
 
-gulp.task('html', ['html:clean', 'bower'], function() {
-  return gulp.src(config.paths.app + '/**/*.html')
-    .pipe(wiredep({ignorePath: '../'+config.paths.build+'/'}))
-    .pipe(gulpif(gutil.env.env === 'production', 
-      function() {
-        if( process.env.GA_TRACKING_ID ) {
-          return googleAnalytics({
-            url: 'labs.atsid.com/nibrs-rfi',
-            uid: process.env.GA_TRACKING_ID,
-            tag: 'body'
-          });
-        } else {
-          gutil.log(gutil.colors.red('You need to set GA_TRACKING_ID environment variable!'));
-          gutil.beep();
-          return gutil.noop();
-        }
-      }
-    ))
-    .pipe(gulp.dest(config.paths.build));
+gulp.task('html', ['html:clean', 'bower'], function () {
+    return gulp.src(config.paths.app + '/**/*.html')
+        .pipe(gulpif(gutil.env.env === 'production',
+            function () {
+                if (process.env.GA_TRACKING_ID) {
+                    return googleAnalytics({
+                        url: 'labs.atsid.com/nibrs-rfi',
+                        uid: process.env.GA_TRACKING_ID,
+                        tag: 'body'
+                    });
+                }
+                else {
+                    gutil.log(gutil.colors.red('You need to set GA_TRACKING_ID environment variable!'));
+                    gutil.beep();
+                    return gutil.noop();
+                }
+            }
+        ))
+        .pipe(gulp.dest(config.paths.build));
 });
 
 // Developing
 gulp.task('serve', function () {
-  
+
     browserSync({
         notify: false,
         online: true,
-        port: 9000,
+        port  : 9000,
         server: {
             baseDir: ['src'],
-            routes: {
+            routes : {
                 '/bower_components': 'bower_components'
             }
         }
     });
 
-  gulp.watch([
-    'src/*.html',
-    'src/js/**/*.js',
-    'src/img/**/*'
-  ]).on('change', reload);
+    gulp.watch([
+        'src/*.html',
+        'src/js/**/*.js',
+        'src/img/**/*'
+    ]).on('change', reload);
 
-  gulp.watch('src/css/**/*.css');
-  gulp.watch('bower.json', ['wiredep']);
+    gulp.watch('src/css/**/*.css');
+    gulp.watch('bower.json', ['wiredep']);
 });
 
 // Deploy
-gulp.task('deploy', [], function() {
-  return gulp.src(config.paths.build + '/**/*')
-    .pipe(ghPages());
+gulp.task('deploy', [], function () {
+    return gulp.src(config.paths.build + '/**/*')
+        .pipe(ghPages());
 });
 
-gulp.task('build', ['styles','js','bower','html','images']);
+gulp.task('build', ['styles', 'js', 'bower', 'html', 'images']);
 gulp.task('default', ['build']);
